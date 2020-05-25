a46e8344706a4540b6fbed3158f501ba18a400b3
Tobias Maier <maiertobias@outlook.com>
2020-05-25 11:46:16 +0200

    Create single main.ts file

--- public/insert-html.ts
+++
@@ -1,6 +0,0 @@
-document.querySelector('#insert-html__submit').addEventListener('click', (e) => {
-  const textarea: HTMLTextAreaElement = document.querySelector('#insert-html__textarea');
-  const clipboard: HTMLElement = document.querySelector('#clipboard');
-  let textareaContent: string = textarea.value;
-  textarea.value.indexOf('data-editable=""') == -1 ? clipboard.innerHTML = `<div data-editable="">${textarea.value}</div>` : clipboard.innerHTML = textarea.value;
-});
--- public/main.ts
+++ public/main.ts
@@ -1,10 +1,3 @@
-import { copy } from './copy';
-import { switcher } from './base-switcher';
-
-copy();
-switcher();
-
-
 class ImageHandler {
   allImages: NodeListOf<Element>;
   activeImage: any;
@@ -58,3 +51,64 @@ interface Image {
 }
 
 let images = new ImageHandler(document.querySelectorAll('[data-editable-image]')).attachEventListener();
+
+// Switcher
+document.querySelector('#base').addEventListener('change', (e) => {
+  updateContent();
+});
+document.querySelector('#num-columns').addEventListener('change', (e) => {
+  updateContent();
+});
+
+function updateContent() {
+  let content: string;
+  const columnWrapper: HTMLSelectElement = document.querySelector('.dialog__columns');
+  const numColumns: number = parseInt((document.querySelector('#num-columns') as HTMLInputElement).value);
+  const switcher: number = parseInt((document.querySelector('#base') as HTMLInputElement).value);
+
+  if (switcher == 1) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open('GET', 'https://tobsfit.github.io/surfooter-generator/simple-surfooter.html', false);
+    xmlhttp.send();
+    content = xmlhttp.responseText;
+    columnWrapper.style.display = "none";
+  } else {
+    columnWrapper.style.display = "block";
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open('GET', `https://tobsfit.github.io/surfooter-generator/complex-surfooter-${numColumns}.html`, false);
+    xmlhttp.send();
+    content = xmlhttp.responseText;
+  }
+  document.querySelector('#clipboard').innerHTML = content;
+  console.log('insert content from switcher');
+}
+
+// Copy Code
+document.querySelector('#copy-content-clipboard').addEventListener('click', () => {
+  console.log('copy-content-clipboard');
+  var clipboardContent = document.getElementById("clipboard").innerHTML;
+  copySurfooterMarkup(clipboardContent);
+
+  function copySurfooterMarkup(str) {
+    let markup = str
+    markup = markup.replace(/ data-editable=""/g, '').replace(/ data-editable-image=""/g, '');
+
+    document.addEventListener('copy', listener);
+    document.execCommand('copy');
+    document.removeEventListener('copy', listener);
+
+    function listener(e) {
+      e.clipboardData.setData('text/html', markup);
+      e.clipboardData.setData('text/plain', markup);
+      e.preventDefault();
+    }
+  }
+});
+
+// Insert html code via textarea
+document.querySelector('#insert-html__submit').addEventListener('click', (e) => {
+  const textarea: HTMLTextAreaElement = document.querySelector('#insert-html__textarea');
+  const clipboard: HTMLElement = document.querySelector('#clipboard');
+  let textareaContent: string = textarea.value;
+  textarea.value.indexOf('data-editable=""') == -1 ? clipboard.innerHTML = `<div data-editable="">${textarea.value}</div>` : clipboard.innerHTML = textarea.value;
+});
