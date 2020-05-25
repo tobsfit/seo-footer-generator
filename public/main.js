76e1a1775429b17d298ea8e956efb27f3e9efb97
Tobias Maier <maiertobias@outlook.com>
2020-05-25 11:46:43 +0200

    Updated main.js file

--- public/main.js
+++ public/main.js
@@ -1,9 +1,3 @@
-"use strict";
-exports.__esModule = true;
-var copy_1 = require("./copy");
-var base_switcher_1 = require("./base-switcher");
-copy_1.copy();
-base_switcher_1.switcher();
 var ImageHandler = /** @class */ (function () {
     function ImageHandler(image) {
         this.image = image;
@@ -44,3 +38,57 @@ var ImageHandler = /** @class */ (function () {
     return ImageHandler;
 }());
 var images = new ImageHandler(document.querySelectorAll('[data-editable-image]')).attachEventListener();
+// Switcher
+document.querySelector('#base').addEventListener('change', function (e) {
+    updateContent();
+});
+document.querySelector('#num-columns').addEventListener('change', function (e) {
+    updateContent();
+});
+function updateContent() {
+    var content;
+    var columnWrapper = document.querySelector('.dialog__columns');
+    var numColumns = parseInt(document.querySelector('#num-columns').value);
+    var switcher = parseInt(document.querySelector('#base').value);
+    if (switcher == 1) {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.open('GET', 'https://tobsfit.github.io/surfooter-generator/simple-surfooter.html', false);
+        xmlhttp.send();
+        content = xmlhttp.responseText;
+        columnWrapper.style.display = "none";
+    }
+    else {
+        columnWrapper.style.display = "block";
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.open('GET', "https://tobsfit.github.io/surfooter-generator/complex-surfooter-" + numColumns + ".html", false);
+        xmlhttp.send();
+        content = xmlhttp.responseText;
+    }
+    document.querySelector('#clipboard').innerHTML = content;
+    console.log('insert content from switcher');
+}
+// Copy Code
+document.querySelector('#copy-content-clipboard').addEventListener('click', function () {
+    console.log('copy-content-clipboard');
+    var clipboardContent = document.getElementById("clipboard").innerHTML;
+    copySurfooterMarkup(clipboardContent);
+    function copySurfooterMarkup(str) {
+        var markup = str;
+        markup = markup.replace(/ data-editable=""/g, '').replace(/ data-editable-image=""/g, '');
+        document.addEventListener('copy', listener);
+        document.execCommand('copy');
+        document.removeEventListener('copy', listener);
+        function listener(e) {
+            e.clipboardData.setData('text/html', markup);
+            e.clipboardData.setData('text/plain', markup);
+            e.preventDefault();
+        }
+    }
+});
+// Insert html code via textarea
+document.querySelector('#insert-html__submit').addEventListener('click', function (e) {
+    var textarea = document.querySelector('#insert-html__textarea');
+    var clipboard = document.querySelector('#clipboard');
+    var textareaContent = textarea.value;
+    textarea.value.indexOf('data-editable=""') == -1 ? clipboard.innerHTML = "<div data-editable=\"\">" + textarea.value + "</div>" : clipboard.innerHTML = textarea.value;
+});
